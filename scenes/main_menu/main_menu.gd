0b56b45507b0d1d9c54c8a44b511f170ae50e2f5
Darwyi <zhenya20022008@gmail.com>
2026-03-26 07:59:40 +0100

    Fix, optimizations

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,43 +1,39 @@
 extends Control
 
-@onready var game_buttons = $GameButtons
-@onready var main_menu_buttons = $main_menu_buttons
+@onready var buttons = $Buttons
 @onready var Settings_scene = preload("res://scenes/main_menu/Settings.tscn")
 @onready var Game_scene = preload("res://scenes/wires_game/WiresGame.tscn")
 var animation
 
 var tweens := {}
+var original_scales := {}
 
 func _ready() -> void:
-	for child in game_buttons.get_children():
+	for child in buttons.get_children():
 		if child is Button:
 			child.pivot_offset = child.size / 2
 			child.mouse_entered.connect(_on_button_mouse_entered.bind(child))
 			child.mouse_exited.connect(_on_button_mouse_exited.bind(child))
-			child.scale = Vector2(1.0, 1.0)
 			
-	for child in main_menu_buttons.get_children():
-		if child is Button:
-			child.pivot_offset = child.size / 2
-			child.mouse_entered.connect(_on_button_mouse_entered.bind(child))
-			child.mouse_exited.connect(_on_button_mouse_exited.bind(child))
-			child.scale = Vector2(1.0, 1.0)
+			child.scale -= Vector2(0.2, 0.2)
+			original_scales[child] = child.scale
 
-
-func _process(delta: float) -> void:
-	pass
-	
 func _on_button_mouse_entered(button: Button):
-	
 	if tweens.has(button) and tweens[button].is_valid():
 		tweens[button].kill()
+		
+	$AudioStreamPlayer2D.play()
 	
 	animation = get_tree().create_tween().set_loops()
-	
 	tweens[button] = animation
-	
-	animation.parallel().tween_property(button, "scale", Vector2(0.9, 0.9), 0.7)
-	animation.parallel().tween_property(button, "rotation", deg_to_rad(10), 0.7)
+	animation.parallel().tween_property(
+		
+		button, "scale", Vector2(original_scales[button].x - 0.2, original_scales[button].y - 0.2), 
+		0.7)
+		
+	animation.parallel().tween_property(
+		button, "rotation", deg_to_rad(10),
+		 0.7)
 	animation.tween_property(button, "rotation", deg_to_rad(-10), 0.7)
 	
 func _on_button_mouse_exited(button: Button):
@@ -46,31 +42,27 @@ func _on_button_mouse_exited(button: Button):
 	
 	animation = get_tree().create_tween()
 	tweens[button] = animation
-	animation.tween_property(button, "scale", Vector2(1.0, 1.0), 0.7)
+	
+	animation.tween_property(
+		button, "scale", Vector2(original_scales[button].x, original_scales[button].y),
+		 0.7)
 	animation.tween_property(button, "rotation", deg_to_rad(0), 0.7)
 	animation.chain()
 
-
 func _on_new_game_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
-	await $AudioStreamPlayer2D.finished
 	get_tree().change_scene_to_packed(Game_scene)
 	
 
 func _on_saved_games_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
 
 
 func _on_exit_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
 	get_tree().quit()
 
 func _on_settings_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
 	var settings = Settings_scene.instantiate()
 	add_child(settings)
 
 func _on_game_library_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
